8ba32f4eec3330de5b8dcb0d5fd828bc0f415a9c
Svetlana Govyazina <govyazina@gmail.com>
2023-01-19 19:24:13 +0200

    materialize added. input for user dates added

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-var Chart = require('chart.js/auto');
+const Chart = require('chart.js/auto');
+const Materialize = require('materialize-css/dist/js/materialize.min');
+let chart = {};
 
-const ctx = document.getElementById('myChart');
+document.addEventListener('DOMContentLoaded', function () {
+    let startDate;
+    let endDate;
+    const inputStart = document.querySelectorAll('.datepicker-start');
+    const inputEnd = document.querySelectorAll('.datepicker-end');
+    M.Datepicker.init(inputStart, {
+        onSelect: (date) => {
+            startDate = date;
+            if (startDate && endDate) {
+                fetchData(startDate, endDate).then(data => changeChart(data));
+            }
+        },
+        autoClose: true,
+    });
+    M.Datepicker.init(inputEnd, {
+        onSelect: (date) => {
+            endDate = date;
+            if (startDate && endDate) {
+                fetchData(startDate, endDate).then(data => changeChart(data));
+            }
+        },
+        autoClose: true,
+    });
+
+
+});
 
 
 (async () => {
-    const data = await fetch('https://api.open-meteo.com/v1/forecast?latitude=34.68&longitude=33.04&daily=temperature_2m_max,temperature_2m_min&timezone=auto&past_days=7').then(resp => resp.json()).then(res => res.daily);
-    console.log(data)
+    const now = new Date();
+    now.setDate(now.getDate() + 7);
+    const data = await fetchData(new Date(), now);
+    showChart(data);
 
-    new Chart(ctx, {
+})();
+
+function showChart(data) {
+    const ctx = document.getElementById('myChart');
+    chart = new Chart(ctx, {
         type: 'bar',
         data: {
             labels: data.time,
@@ -16,11 +49,11 @@ const ctx = document.getElementById('myChart');
                 data: data.temperature_2m_max,
                 borderWidth: 1
             },
-            {
-                label: 'температура мин',
-                data: data.temperature_2m_min,
-                borderWidth: 1
-            }]
+                {
+                    label: 'температура мин',
+                    data: data.temperature_2m_min,
+                    borderWidth: 1
+                }]
         },
         options: {
             scales: {
@@ -30,5 +63,27 @@ const ctx = document.getElementById('myChart');
             }
         }
     });
-}) ();
+
+}
+
+function changeChart(data) {
+    chart.data.labels = data.time;
+    chart.data.datasets[0].data = data.temperature_2m_max;
+    chart.data.datasets[1].data = data.temperature_2m_min;
+    chart.update();
+}
+
+function fetchData(startDate, endDate) {
+    const url = new URL('https://api.open-meteo.com/v1/forecast');
+    url.searchParams.set('latitude', '34.68');
+    url.searchParams.set('longitude', '33.04');
+    url.searchParams.set('longitude', '33.04');
+    url.searchParams.set('daily', 'temperature_2m_max');
+    url.searchParams.append('daily', 'temperature_2m_min');
+    url.searchParams.set('timezone', 'auto');
+    url.searchParams.set('start_date', startDate.toISOString().slice(0, 10));
+    url.searchParams.set('end_date', endDate.toISOString().slice(0, 10));
+    return fetch(url).then(resp => resp.json()).then(res => res.daily);
+
+}
 
